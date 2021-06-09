92af380785a3cb4784f83601be8c04f6faddb527
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-06-09 17:43:51 +0100

    Tensors and variables

--- src/tensor-and-variable.py
+++ src/tensor-and-variable.py
@@ -1,6 +1,4 @@
-import numpy as np
 import tensorflow as tf
-
 
 v1 = tf.Variable([[1.5, 2, 5], [2, 6, 7]])
 print(v1)
@@ -17,5 +15,16 @@ print(tf.convert_to_tensor(v1))
 
 print(v4.numpy())
 
-v1[0,0].assign(100)
+v1[0, 0].assign(100)
 print(v1.numpy())
+
+v1.assign_add([[1, 1, 1], [1, 1, 1]])
+print(v1)
+
+var_a = tf.Variable([2.0, 3.0])
+var_b = tf.Variable(var_a)
+print(var_b)
+
+var_b.assign([200,300])
+print(var_a.numpy())
+print(var_b.numpy())
