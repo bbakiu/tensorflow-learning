e99e560dc00f4bb8ad754751967c93e057ab1c45
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-06-11 14:56:36 +0100

    Eager execution

--- src/eager-execution.py
+++ src/eager-execution.py
@@ -20,6 +20,26 @@ c = tf.Variable([1.0, 1.0, 1.0], tf.float32, name='c')
 
 x = tf.Variable([100.0, 100.0, 100.0], tf.float32, name='x')
 
-y = m*c + x
+y = m * c + x
 
 print(y)
+
+
+def tensorflow_div(max_num_int):
+    counter = tf.constant(0)
+    max_num = tf.constant(max_num_int)
+
+    for num in range(0, max_num.numpy() + 1):
+        if int(num % 3) == 0 and int(num % 5) == 0:
+            print('Divisible by 3 and 5: ', num)
+        elif int(num % 3) == 0:
+            print('Divisible by 3 ', num)
+        elif int(num % 5) == 0:
+            print('Divisible by 5 ', num)
+        else:
+            print(num)
+
+        counter += 1
+
+
+tensorflow_div(15)
