d9d75fc5187bb0ba694e18178b1ef373ceea918a
Bujar Bakiu <bakiu.bujar@gmail.com>
2021-06-21 08:33:50 +0200

    Keras API

--- src/regression-keras.py
+++ src/regression-keras.py
@@ -10,9 +10,12 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.callbacks import TensorBoard
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+import tensorflow_docs as tfdocs
+import tensorflow_docs.modeling
 
 data = pd.read_csv('../dataset/insurance.csv')
 print(data.describe().T)
@@ -49,7 +52,7 @@ print(categorical_features.shape)
 
 
 standard_scaler = StandardScaler()
-numerical_features =pd.DataFrame(standard_scaler.fit_transform(numerical_features), columns=numerical_features.columns, index=numerical_features.index)
+numerical_features = pd.DataFrame(standard_scaler.fit_transform(numerical_features), columns=numerical_features.columns, index=numerical_features.index)
 print(numerical_features.describe().T)
 
 processed_features = pd.concat([numerical_features, categorical_features], axis=1, sort=False)
@@ -57,3 +60,89 @@ print(processed_features.head())
 processed_data = pd.concat([processed_features, target], axis=1, sort=False)
 
 processed_data.to_csv('../dataset/insurance_processed.csv', index=False)
+
+X_train, X_test, y_train, y_test = train_test_split(processed_features, target, test_size=0.2, random_state=101)
+
+model = Sequential()
+model.add(Dense(32, activation='relu', input_shape=[len(X_train.keys())]))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(1))
+
+optimizer = Adam(0.001)
+model.compile(loss='mse', optimizer=optimizer, metrics=['mae', 'mse'])
+
+print(model.summary())
+
+epochs = 500
+# history = model.fit(X_train, y_train, epochs=epochs, validation_split=0.2, verbose=True )
+#
+# history_df = pd.DataFrame(history.history)
+# history_df['epoch'] = history.epoch
+#
+# print(history_df.tail())
+#
+# predictions = model.predict(X_test)
+# print(predictions)
+# print(predictions.flatten())
+#
+# plt.scatter(y_test, predictions.flatten(), s=200, c='darkblue')
+# plt.show()
+#
+# print(r2_score(y_test, predictions ))
+
+model_elu = Sequential()
+model_elu.add(Dense(32, activation='elu', input_shape=[len(X_train.keys())]))
+model_elu.add(Dropout(0.2))
+model_elu.add(Dense(64, activation='elu'))
+model_elu.add(Dense(1))
+
+optimizer = Adam(0.001)
+model_elu.compile(loss='mse', optimizer=optimizer, metrics=['mae', 'mse'])
+
+print(model_elu.summary())
+
+# history = model_elu.fit(X_train, y_train, epochs=epochs, validation_split=0.2, verbose=True, callbacks=[tfdocs.modeling.EpochDots()])
+#
+# history_df = pd.DataFrame(history.history)
+# history_df['epoch'] = history.epoch
+#
+# print(history_df.tail())
+#
+# predictions = model_elu.predict(X_test).flatten()
+# plt.scatter(y_test, predictions, s=200, c='darkblue')
+# plt.show()
+#
+# print(r2_score(y_test, predictions))
+
+
+model_elu_ES = Sequential()
+model_elu_ES.add(Dense(32, activation='elu', input_shape=[len(X_train.keys())]))
+model_elu_ES.add(Dropout(0.2))
+model_elu_ES.add(Dense(64, activation='elu'))
+model_elu_ES.add(Dense(1))
+
+optimizer = Adam(0.001)
+model_elu_ES.compile(loss='mse', optimizer=optimizer, metrics=['mae', 'mse'])
+
+print(model_elu_ES.summary())
+
+early_stop = EarlyStopping(monitor='val_loss', patience=5)
+
+history = model_elu_ES.fit(X_train, y_train, epochs=epochs, validation_split=0.2, verbose=True, callbacks=[early_stop, tfdocs.modeling.EpochDots()])
+
+history_df = pd.DataFrame(history.history)
+history_df['epoch'] = history.epoch
+
+print(history_df.tail())
+
+predictions = model_elu_ES.predict(X_test).flatten()
+plt.scatter(y_test, predictions, s=200, c='darkblue')
+plt.show()
+
+print(r2_score(y_test, predictions))
+
+model_elu_ES.save('./models/relu_es.h5', save_format='h5')
+model_elu_ES.save_weights('./models/relu_es_weights.h5', save_format='h5')
+
+
+print(pprint.pprint(json.loads(model_elu_ES.to_json())))
